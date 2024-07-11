5231fec74b21885932d2b481c296da667f02d406
Rohit Sharma <rohits8731@gmail.com>
2024-07-11 20:05:14 +0000

    first commit

--- script.js
+++ script.js
@@ -1,46 +1,81 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#wrapper input");
+const tasksContainer = document.querySelector("#tasks");
+const error = document.getElementById("error");
+const countValue = document.querySelector(".count-value");
 
-function addTask()
-{
-    if(inputBox.value === '')
+let taskCount = 0;
+const displayCount = (taskCount) =>{
+    countValue.innerText = taskCount;
+};
+
+const addTask = () =>{
+    const taskName = newTaskInput.value.trim();
+    if(!taskName)
     {
-        alert("You Must Write Something!");
+        setTimeout(() =>{
+            error.style.display = "block";
+        },200)
+        return;
     }
-    else
-    {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = "";
-    saveData();
+    const task = `<div class="task">
+                <input type = "checkbox" class="task-check">
+                <span class="taskname">${taskName}</span>
+                <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+                <button class="delete"><i class="fa-solid fa-trash"></i></button>
+                </div>`;
+    
+    tasksContainer.insertAdjacentHTML("beforeend",task);
+
+    const deleteButtons = document.querySelectorAll(".delete");
+    deleteButtons.forEach((button)=>{
+        button.onclick = () =>{
+            button.parentNode.remove();
+            taskCount-=1;
+            displayCount(taskCount);
+        };
+    });
+
+    const editButtons = document.querySelectorAll(".edit");
+    editButtons.forEach((button)=>{
+        button.onclick = (e) =>{
+            let targetElement = e.target;
+            if(!(e.target.className == "edit"))
+            {
+                targetElement = e.target.parentElement;
+            }
+            newTaskInput.value = targetElement.previousElementSibling?.innerText;
+            targetElement.parentNode.remove();
+            taskCount-=1;
+            displayCount(taskCount);
+        };
+    });
+
+    const tasksCheck = document.querySelectorAll(".task-check");
+    tasksCheck.forEach((checkbox)=>{
+        checkbox.onchange = () =>{
+            checkbox.nextElementSibling.classList.toggle("completed");
+            if(checkbox.checked)
+            {
+                taskCount-=1;
+            }else
+            {
+                taskCount+=1;
+            }
+            displayCount(taskCount);
+        };
+    }); 
+    taskCount+=1;
+    displayCount(taskCount);
+    newTaskInput.value = "";
+};
+
+addBtn.addEventListener("click",addTask);
+
+window.onload = () =>{
+    taskCount = 0;
+    displayCount(taskCount);
+    newTaskInput.value = "";
 }
 
-listContainer.addEventListener("click",function(e)
-{
-    if(e.target.tagName === "LI")
-    {
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    else if(e.target.tagName === "SPAN")
-    {
-        e.target.parentElement.remove();
-        saveData();
-    }
-},false);
 
-function saveData()
-{
-    localStorage.setItem("data",listContainer.innerHTML);
-}
-
-function showTask()
-{
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-showTask();
